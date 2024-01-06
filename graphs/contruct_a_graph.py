7fba019edc26506b943adac225137d40237be198
crpto genesis <prishankifficial@gmail.com>
2024-01-06 18:26:36 +0530

    graph breadth first search

--- graphs/contruct_a_graph.py
+++ graphs/contruct_a_graph.py
@@ -1,16 +1,59 @@
 from collections import defaultdict
 
+# in breadth-first search there are two things to consider visited and queue 
+"""
+    first put data inside the queue and then push inside the visited 
+    pop the queue from start index and visit all it neighbors and if push in the queue 
+    if the neighbor is not visited
+    when the queues is empty exit the loops
+    
+"""
 
+
+
+# * Graph representation using adjacency list
 class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list) # mapping of graphs nodes
 
-    def addEdges (self,u,v):
+    def __init__(self):
+        self.graph =defaultdict(list)
+
+    def addEdge(self,u,v):
         self.graph[u].append(v)
 
-if __name__== "__main__":
-    g= Graph()
-    
+    def BFS(self,s):
+        visited = [False] *(max(self.graph)+1)
+        queue = []
 
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop()
+            print(s,end=" ")
+
+            for i in self.graph[s]:
+                if visited[i] is False:
+                    visited[i] = True
+                    queue.append(i)
+
+
+
+
+if __name__ == "__main__":
+
+    g= Graph()
+    g.addEdge(0,1)
+    g.addEdge(0,2)
+    g.addEdge(1,2)
+    g.addEdge(2,0)
+    g.addEdge(2,3)
+    g.addEdge(3,3)
+
+
+    print(f"graph is {g.graph}")
+
+    searchIndex = 2
+    g.BFS(searchIndex)
 
 
